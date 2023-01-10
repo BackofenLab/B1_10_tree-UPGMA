c91951944b6a0369193891dd68922c4164dde927
domonik <schdruzzi@gmail.com>
2023-01-10 16:55:13 +0100

    only programming

--- exercise_sheet10.py
+++ exercise_sheet10.py
@@ -1,142 +1,5 @@
 from math import inf
 
-
-def exercise_1a():
-    """
-    Use the distance matrix from the Readme to select which leaves should be
-    joined for an internal node first.
-    """
-    one = "c and d"
-    two = "a and b"
-    three = "d and e"
-    return None
-
-
-def exercise_1b():
-    """
-    Calculate the corresponding distance from the leaves to the internal Node
-    selected in a.
-    """
-    distance = None
-    return distance
-
-
-def exercise_1c() -> int:
-    """
-    Which of the distance matrices represents the new one including the
-    internal node from a.
-    Return either 1, 2 or 3
-    """
-    return None
-
-
-def exercise_1d():
-    """
-    Which nodes are joined next given the correct distance matrix from c?
-    """
-    one = "c and d"
-    two = "{a,b} and e"
-    three = "{c,d} and e"
-    four = "e and a"
-    return None
-
-
-def exercise_1e():
-    """
-    Fill in the following distance matrix. (both, upper and lower triangle)
-    """
-    #    {a,b}, {c,d}, e
-    matrix = [
-            [0,    0,  0],  # {a,b}
-            [0,    0,  0],  # {c,d}
-            [0,    0,  0],  # e
-    ]
-    return matrix
-
-
-def exercise_1f():
-    """
-    What does the part of the tree look like in newick format after selecting
-    and joining your answer from e)
-    """
-    one = "((c : 3, d : 3) : 3.5, e : 3.5)"
-    two = "((c : 3, d : 3) : 0.5, e : 3.5)"
-    three = "((a : 1.5, b : 1.5) : 2.75, e : 4.25)"
-    return None
-
-
-def exercise_1g():
-    """
-    Given the initial distance matrix and the WPGMA function, which tree
-    representation is correct?
-    """
-    one = "((a : 1.5, b : 1.5) : 4, ((c : 3, d : 3) : 0.5, e : 3.5) : 2);"
-    two = "((a : 1.5, b : 1.5) : 4.25, ((c : 3, d : 3) : 0.5, e : 3.5) : 2.25);"
-    three = "(((c : 3, d : 3) : 3.5, e : 3.5): 4, (a : 1.5, b : 1.5) : 2);"
-    return None
-
-
-def exercise_2a():
-    """
-    Imagine using  UPGMA instead of WPGMA for construction of a tree.
-    Which of the following statements is True?
-    """
-    # There will only be a difference in edge lengths. Overall topology will
-    # stay the same.
-    one = None
-
-    # The tree in Exercise 1 will not change
-    two = None
-
-    # UPGMA is equal to WPGMA if the number of leaves in the two clusters
-    # (|c| and |d|) is the same.
-    three = None
-
-    # UPGMA can end up with wrong topologies when using non-ultrametric
-    # distances.
-    four = None
-
-    return one, two, three, four
-
-def exercise_3a():
-    """
-    Which of the following distance matrices are ultrametric?
-
-    If a matrix is ultrametric label it as True, otherwise use False.
-    """
-
-
-    #a | 0 | 3 | 12 | 12 |  9 |
-    #b |   | 0 | 13 | 13 | 10 |
-    #c |   |   |  0 |  6 |  7 |
-    #d |   |   |    |  0 |  7 |
-    #e |   |   |    |    |  0 |
-    a = None
-
-    #a | 0 | 2 | 4 | 6 | 8 |
-    #b |   | 0 | 4 | 6 | 8 |
-    #c |   |   | 0 | 6 | 8 |
-    #d |   |   |   | 0 | 8 |
-    #e |   |   |   |   | 0 |
-    b = None
-
-    #a | 0 | 10 | 17 | 16 | 16 |
-    #b |   |  0 | 15 | 14 | 14 |
-    #c |   |    |  0 |  9 | 15 |
-    #d |   |    |    |  0 | 14 |
-    #e |   |    |    |    |  0 |
-    c = None
-
-    #a | 0 | 2 | 2 | 2 | 2 |
-    #b |   | 0 | 4 | 4 | 4 |
-    #c |   |   | 0 | 6 | 6 |
-    #d |   |   |   | 0 | 8 |
-    #e |   |   |   |   | 0 |
-    d = None
-
-    return a, b, c, d
-
-
 ########################################################
 ############## Programming tasks #######################
 ########################################################
@@ -145,49 +8,49 @@ from helpers import compute_distance
 from helpers import Node
 from helpers import Tree
 
+def tutorial():
+    """
+    In all the tasks we will use the same structure of the given data which can be seen as three parts:
+    1. matrix of distances between nodes
+    2. names of the nodes
+    3. wpgma/upgma mode
 
-"""
-In all of the tasks we will use the same structure of the given data which can be seen as three parts:
-1. matrix of distances between nodes
-2. names of the nodes
-3. wpgma/upgma mode
-
-These three components are merged into a "distance info" tuple for our convenience.
-"""
-matrix_dist_1 = [[0, 3, 12, 12, 9], [3, 0, 13, 13, 10], [12, 13, 0, 6, 7], [12, 13, 6, 0, 7], [9, 10, 7, 7, 0]]
-nodes_1 = ["a", "b", "c", "d", "e"]
-weight_1 = "wpgma"
-distance_info1 = matrix_dist_1, nodes_1, weight_1
+    These three components are merged into a "distance info" tuple for our convenience.
+    """
+    matrix_dist_1 = [[0, 3, 12, 12, 9], [3, 0, 13, 13, 10], [12, 13, 0, 6, 7], [12, 13, 6, 0, 7], [9, 10, 7, 7, 0]]
+    nodes_1 = ["a", "b", "c", "d", "e"]
+    weight_1 = "wpgma"
+    distance_info1 = matrix_dist_1, nodes_1, weight_1
 
 
-"""
-You are provided with one helper function and two helper classes to perform the tree assembly:
-compute_distance - computes the distance between two objects, it works for both Nodes and Trees and for both wpgma and upgma
-modes.
-Node - a helper class to convert initial data into a node
-Tree - a tree representation class which can be printed in Newick's representation
+    """
+    You are provided with one helper function and two helper classes to perform the tree assembly:
+    compute_distance - computes the distance between two objects, it works for both Nodes and Trees and for both wpgma and upgma
+    modes.
+    Node - a helper class to convert initial data into a node
+    Tree - a tree representation class which can be printed in Newick's representation
+    
+    Lets start creating nodes and trees.
+    """
 
-Lets start creating nodes and trees.
-"""
+    a = Node("a", distance_info1)
+    b = Node("b", distance_info1)
+    first_tree = a + b
+    print("Nodes a and b", a, b)
+    print("Distance between nodes a and b is", compute_distance(a, b))
+    print(type(a))
+    print("Result of merging two nodes a and is a tree", first_tree)
+    print(type(a+b))
 
-a = Node("a", distance_info1)
-b = Node("b", distance_info1)
-first_tree = a + b
-print("Nodes a and b", a, b)
-print("Distance between nodes a and b is", compute_distance(a, b))
-print(type(a))
-print("Result of merging two nodes a and is a tree", first_tree)
-print(type(a+b))
+    c = Node("c", distance_info1)
+    d = Node("d", distance_info1)
+    second_tree = c + d
+    print("Distance between nodes c and d is", compute_distance(c, d))
+    print("Result of merging two nodes c and is d tree", second_tree)
 
-c = Node("c", distance_info1)
-d = Node("d", distance_info1)
-second_tree = c + d
-print("Distance between nodes c and d is", compute_distance(c, d))
-print("Result of merging two nodes c and is d tree", second_tree)
-
-merged_trees = first_tree + second_tree
-print("Distance between trees  (a,b) and (c,d) is", compute_distance(first_tree, second_tree))
-print("Result of merging together two trees", merged_trees)
+    merged_trees = first_tree + second_tree
+    print("Distance between trees  (a,b) and (c,d) is", compute_distance(first_tree, second_tree))
+    print("Result of merging together two trees", merged_trees)
 
 
 def convert_to_nodes(distance_info):
@@ -220,3 +83,7 @@ def build_the_tree(distance_info):
     """
     tree = None
     return tree
+
+
+if __name__ == '__main__':
+    tutorial()
--- tests/test_exercise_sheet10.py
+++ tests/test_exercise_sheet10.py
@@ -5,68 +5,6 @@ from exercise_sheet10 import *
 from helpers import *
 
 
-def test_exercise_1a():
-    answer = exercise_1a()
-    expected = "a and b"
-    assert answer == expected
-
-
-def test_exercise_1b():
-    answer = exercise_1b()
-    expected = 1.5
-    assert answer == expected
-
-
-def test_exercise_1c():
-    answer = exercise_1c()
-    expected = 1
-    assert answer == expected
-
-
-def test_exercise_1d():
-    answer = exercise_1d()
-    expected = "c and d"
-    assert answer == expected
-
-
-def test_exercise_1e():
-    answer = exercise_1e()
-    matrix = [
-        [   0, 12.5, 9.5],  # {a,b}
-        [12.5,    0, 7],  # {c,d}
-        [   9.5,    7, 0],  # e
-    ]
-    assert answer == matrix
-
-
-def test_exercise_1f():
-    answer = exercise_1f()
-    expected = "((c : 3, d : 3) : 0.5, e : 3.5)"
-    assert answer == expected
-
-
-def test_exercise_1g():
-    answer = exercise_1g()
-    expected = "((a : 1.5, b : 1.5) : 4, ((c : 3, d : 3) : 0.5, e : 3.5) : 2);"
-    assert answer == expected
-
-
-def test_exercise_2a():
-    one, two, three, four = exercise_2a()
-    assert one is False
-    assert two is False
-    assert three is True
-    assert four is True
-
-
-def test_exercise_3a():
-    a, b, c, d = exercise_3a()
-    assert a is False
-    assert b is True
-    assert c is False
-    assert d is False
-
-
 @pytest.fixture
 def distance_info1():
     matrix_dist_1 = [
